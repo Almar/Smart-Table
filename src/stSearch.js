312ef38b1c9240cda57855e61e08b0a7e1a20583
Almar <almarmail-info@yahoo.com>
2015-04-01 22:15:28 +0200

    st-search IE9 support

--- src/stSearch.js
+++ src/stSearch.js
@@ -9,6 +9,7 @@ ng.module('smart-table')
                 var tableCtrl = ctrl;
                 var promise = null;
                 var throttle = attr.stDelay || 400;
+                var currVal;
                 var filter = ctrl.registerFilter('search');
 
                 scope.$watch('predicate', function (newValue, oldValue) {
@@ -30,8 +31,15 @@ ng.module('smart-table')
                 }, true);
 
                 // view -> table state
-                element.bind('input', function (evt) {
+                // 'keyup' and currVal check where added for IE9 support.
+                element.bind('input keyup', function (evt) {
                     evt = evt.originalEvent || evt;
+
+                    // IE9 support. Added because extra event 'keyup'.
+                    var val = evt.target.value;
+                    if (val === currVal) return;
+                    currVal = val;
+
                     if (promise !== null) {
                         $timeout.cancel(promise);
                     }
